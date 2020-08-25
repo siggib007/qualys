670f262ed1a79e4ff423f845a6ed26ece5710c54
Siggi Bjarnason <siggi.bjarnason@t-mobile.com>
2020-08-25 15:42:55 +0000

    Code Cleanup

--- QualysHostListFile.py
+++ QualysHostListFile.py
@@ -15,7 +15,6 @@ import time
 import xmltodict
 import urllib.parse as urlparse
 import xml.parsers.expat
-import json
 import platform
 # End imports
 
@@ -38,13 +37,6 @@ iLoc = strScriptName.rfind(".")
 strLogFile = strLogDir + "/" + strScriptName[:iLoc] + ISO + ".log"
 strRealPath = os.path.realpath(sys.argv[0])
 strVersion = "{0}.{1}.{2}".format(sys.version_info[0],sys.version_info[1],sys.version_info[2])
-localtime = time.localtime(time.time())
-gmt_time = time.gmtime()
-iGMTOffset = (time.mktime(localtime) - time.mktime(gmt_time))/3600
-
-strScriptHost = platform.node().upper()
-if strScriptHost == "DEV-APS-RHEL-STD-A":
-  strScriptHost = "VMSAWS01"
 
 print ("This is a script to gather all asset host information from Qualys via API. This is running under Python Version {}".format(strVersion))
 print ("Running from: {}".format(strRealPath))
@@ -53,16 +45,7 @@ print ("The time now is {}".format(now))
 print ("Logs saved to {}".format(strLogFile))
 objLogOut = open(strLogFile,"w",1)
 
-dboErr = None
-dbo = None
 iTotalCount = 0
-iEntryID = 0
-iCountHostChange = 0
-iCountTagChange = 0
-strDBType = "undef"
-strDBUser = ""
-strDBPWD = ""
-strdbNow = time.strftime("%Y-%m-%d %H:%M:%S")
 
 def CleanExit(strCause):
   objLogOut.close()
@@ -81,19 +64,9 @@ def processConf():
   global strHeader
   global strUserName
   global strPWD
-  global strServer
-  global strDBUser
-  global strDBPWD
-  global strInitialDB
-  global iMinQuietTime
-  global strLoadType
-  global dtFullLoad
-  global strNotifyURL
-  global strNotifyToken
-  global strNotifyChannel
-  global strDBType
   global strFileout
   global iBatchSize
+  global strHostIDs
 
   if os.path.isfile(strConf_File):
     LogEntry ("Configuration File exists")
@@ -126,30 +99,10 @@ def processConf():
         strUserName = strValue
       if strVarName == "QUserPWD":
         strPWD = strValue
-      if strVarName == "Server":
-        strServer = strValue
-      if strVarName == "dbUser":
-        strDBUser = strValue
-      if strVarName == "dbPWD":
-        strDBPWD = strValue
-      if strVarName == "InitialDB":
-        strInitialDB = strValue
-      if strVarName == "MinQuietTime":
-        iMinQuietTime = int(strValue)
-      if strVarName == "LoadType":
-        strLoadType = strValue
-      if strVarName == "FullFromDate":
-        dtFullLoad = strValue
-      if strVarName == "NotificationURL":
-        strNotifyURL = strValue
-      if strVarName == "NotifyChannel":
-        strNotifyChannel = strValue
-      if strVarName == "NotifyToken":
-        strNotifyToken = strValue
-      if strVarName == "DBType":
-        strDBType  = strValue
       if strVarName == "OutfileName":
         strFileout  = strValue
+      if strVarName == "HostIDs":
+        strHostIDs  = strValue
       if strVarName == "BatchSize":
         iBatchSize = int(strValue)
 
@@ -217,23 +170,9 @@ def MakeAPICall (strURL, strHeader, strUserName,strPWD, strMethod):
   else:
     return dictResponse
 
-def isInt (CheckValue):
-  # function to safely check if a value can be interpreded as an int
-  if isinstance(CheckValue,int):
-    return True
-  elif isinstance(CheckValue,str):
-    if CheckValue.isnumeric():
-      return True
-    else:
-      return False
-  else:
-    return False
-
 processConf()
 
 LogEntry("Starting Processing. Script {} running under Python version {}".format(strRealPath,strVersion))
-
-strdbNow = time.strftime("%Y-%m-%d %H:%M:%S")
 
 strAPIFunction = "/api/2.0/fo/asset/host/"
 if strAPIFunction[0] == "/":
@@ -254,12 +193,11 @@ strMethod = "get"
 dictParams = {}
 dictParams["action"] = "list"
 dictParams["truncation_limit"] = iBatchSize
-# dictParams["ids"] = "119710152,171444421,129630824,119729204,119729206"
+dictParams["ids"] = strHostIDs
 
 strListScans = urlparse.urlencode(dictParams)
 bMoreData = True
 iTotalCount = 0
-iTotalTagCount = 0
 iCount = 1
 
 strURL = strBaseURL + strAPIFunction +"?" + strListScans
@@ -287,7 +225,6 @@ while bMoreData:
           LogEntry ("{} hosts in results".format(iResultCount))
           # for dictHosts in APIResponse["HOST_LIST_OUTPUT"]["RESPONSE"]["HOST_LIST"]["HOST"]:
           #   UpdateDB (dictHosts)
-          #   iTotalCount += 1
         else:
           iTotalCount += 1
           LogEntry ("Only one host in results")
